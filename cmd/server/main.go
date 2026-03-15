844198d9f1440972c4f64ed3cef450c8270be8dd
Vinícius Nascimento Silva <vinicius.nas.silva@gmail.com>
2026-03-15 15:36:02 -0300

    Init server and change module name

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionUrl := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionUrl)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer connection.Close()
+	fmt.Println("Connected to message broker")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	fmt.Println("Server is running, press ctrl+c to stop")
+
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	<-sigChan
+	fmt.Println("\n ctrl+c received, shutting down")
 }
